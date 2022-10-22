3386ee9d268e29f55bb9147443a00815a8014069
muzammildeveloper <khanmuzammil823@gmail.com>
2022-10-23 01:54:24 +0530

    error founded in the applicaiton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./components/LoginForm";
+import ChatFeed from "./components/ChatFeed";
 
-function App() {
+import "./App.css";
+
+const projectID = "32ec87c9-09c7-4a6f-a97d-5272b03efa4e";
+export function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      projectID={projectID}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
